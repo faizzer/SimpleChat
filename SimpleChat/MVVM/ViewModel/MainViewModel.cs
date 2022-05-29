fc0cde2b1021843bf331a7e13766daedfff44cdc
faizzer <faizzer1234@gmail.com>
2022-05-29 18:37:22 +0300

    ContactCard

---
+++ SimpleChat/MVVM/ViewModel/MainViewModel.cs
@@ -0,0 +1,81 @@
+﻿using SimpleChat.MVVM.Model;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleChat.MVVM.ViewModel
+{
+    class MainViewModel
+    {
+        public ObservableCollection<MessageModel> Messages { get; set; }
+        public ObservableCollection<ContactModel> Contacts { get; set; }
+
+        public MainViewModel()
+        {
+            Messages = new ObservableCollection<MessageModel>();
+            Contacts = new ObservableCollection<ContactModel>();
+
+            Messages.Add(new MessageModel
+            {
+                Username = "Elliot",
+                UsernameColor = "#409aff",
+                ImageSource = "https://i.imgur.com/GM8uZELb.jpg",
+                Message = "Test",
+                Time = DateTime.Now,
+                IsNativeOrigin = false,
+                FirstMessage = true
+            });
+
+            for (int i = 0; i < 3; i++)
+            {
+                Messages.Add(new MessageModel
+                {
+                    Username = "Elliot",
+                    UsernameColor = "#409aff",
+                    ImageSource = "https://i.imgur.com/GM8uZELb.jpg",
+                    Message = "Test",
+                    Time = DateTime.Now,
+                    IsNativeOrigin = false,
+                    FirstMessage = false
+                });
+            }
+
+            for (int i = 0; i < 3; i++)
+            {
+                Messages.Add(new MessageModel
+                {
+                    Username = "John",
+                    UsernameColor = "#409aff",
+                    ImageSource = "https://i.imgur.com/GM8uZELb.jpg",
+                    Message = "Test",
+                    Time = DateTime.Now,
+                    IsNativeOrigin = true
+                });
+            }
+
+            Messages.Add(new MessageModel
+            {
+                Username = "John",
+                UsernameColor = "#409aff",
+                ImageSource = "https://i.imgur.com/GM8uZELb.jpg",
+                Message = "Last",
+                Time = DateTime.Now,
+                IsNativeOrigin = true
+            });
+
+            for (int i = 0; i < 5; i++)
+            {
+                Contacts.Add(new ContactModel
+                {
+                    Username = $"Elliot {i}",
+                    ImageSource = "https://i.imgur.com/amXr0IGb.jpg",
+                    Messages = Messages
+                });
+            }
+
+        }
+    }
+}
